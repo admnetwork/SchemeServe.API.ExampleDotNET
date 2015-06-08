1a39361e648b75b3fa584f39c8515d7adbf5bdeb
kostahristov-schemeserve <kosta.hristov@schemeserve.com>
2015-06-08 14:19:20 +0300

    Committing the .Net API demo client.

---
+++ APISimulator-.Net/APISimulator-.Net/Program.cs
@@ -0,0 +1,34 @@
+﻿using SSClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APISimulator_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tokenURL = "https://{account}.schemeserve.com/api/token/{username}/{password}";
+            string casesAPIEndPoint = "https://{account}.schemeserve.com/api/cases";
+
+            //long demoCaseID = ######;
+
+            string requestBodyPath = "requestBody.xml";
+            string requestResultFilePath = "result.txt";
+
+            var client = new SchemeServeAPIClient(requestBodyPath, requestResultFilePath);
+
+            client.LoadToken(tokenURL);
+
+            client.SubmitCase(casesAPIEndPoint);
+
+            //client.GetCase(casesAPIEndPoint, demoCaseID);
+
+            Console.WriteLine("Success");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ APISimulator-.Net/APISimulator-.Net/SchemeServeAPIClient.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.Text;
+
+namespace SSClient
+{
+    public class SchemeServeAPIClient
+    {
+        public string Token { get; set; }
+        public DateTime TokenExpiryTimestamp { get; set; }
+
+        private string RequestBodyFilePath { get; set; }
+        private string ResultFilePath { get; set; }
+
+        public SchemeServeAPIClient(string requestBodyFilePath, string resultFilePath)
+        {
+            this.RequestBodyFilePath = requestBodyFilePath;
+            this.ResultFilePath = resultFilePath;
+
+            this.Token = String.Empty;
+            this.TokenExpiryTimestamp = DateTime.MinValue;
+        }
+
+        public void LoadToken(string tokenURL)
+        {
+            string tokenRaw = String.Empty;
+
+            try
+            {
+                tokenRaw = this.PerformGetRequest(tokenURL);
+
+                XDocument doc = XDocument.Parse(tokenRaw);
+
+                this.Token = doc.Element("access").Element("token").Value;
+                long ExpiresNum = Convert.ToInt64(doc.Element("access").Element("expires").Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void SubmitCase(string url)
+        {
+            string requestBody = File.ReadAllText(this.RequestBodyFilePath);
+
+            url = String.Format("{0}?token={1}", url, this.Token);
+
+            HttpWebRequest request = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            HttpWebResponse response = null;
+
+            request.Method = "POST";
+            request.Timeout = 20000;
+
+            request.Accept = "application/xml";
+            request.ContentType = "application/xml";
+            request.ContentLength = requestBody.Length;
+
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(requestBody);
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+            }
+
+            string result = String.Empty;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                Encoding respEncoding = Encoding.GetEncoding(response.CharacterSet);
+
+                using (Stream ReceiveStream = response.GetResponseStream())
+                {
+                    using (StreamReader readStream = new StreamReader(ReceiveStream, respEncoding))
+                    {
+                        result = readStream.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+
+            File.WriteAllText(this.ResultFilePath, result);
+        }
+
+        public void GetCase(string getCaseURL, long caseID) {
+            this.PerformGetRequest(getCaseURL + "/" + caseID);
+        }
+
+        private string PerformGetRequest(string url)
+        {
+            url = String.Format("{0}?token={1}", url, this.Token);
+
+            HttpWebRequest request = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            HttpWebResponse response = null;
+
+            request.Method = "GET";
+            request.Timeout = 20000;
+
+            string result = String.Empty;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+
+                Encoding respEncoding = Encoding.GetEncoding(response.CharacterSet);
+
+                using (Stream ReceiveStream = response.GetResponseStream())
+                {
+                    using (StreamReader readStream = new StreamReader(ReceiveStream, respEncoding))
+                    {
+                        result = readStream.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException Ex)
+            {
+                throw Ex;
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+
+            File.WriteAllText(this.ResultFilePath, result);
+
+            return result;
+        }
+
+    }
+}
